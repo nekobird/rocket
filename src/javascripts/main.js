8f8380de09b56fc6566b51c20eb9a1b952ad1612
Andrew Prasetya <andrewprasetya@me.com>
2019-04-08 00:43:24 -0400

    Started wave experiment

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -12,21 +12,81 @@ import {
   Vector2,
 } from './rocket/Rocket'
 
-let el_layer_stack = document.getElementById('layer_stack')
 
+class Wave {
+
+  constructor() {
+    this.amplitude = 60
+    this.bezierLength = 100
+    this.numberOfPoints = 12
+    this.width = 1000
+
+    this.points = new Array
+  }
+
+  initializePoints() {
+    this.points = new Array
+    for (let i = 0; i < this.numberOfPoints; i++) {
+      this.points[i] = new Vector2()
+    }
+    this.generatePoints()
+  }
+
+  generatePoints() {
+    let segment = this.width / this.numberOfPoints
+    this.points.map((p, i) => {
+      let m = 1
+      if (i % 2 === 0) {
+        m = -1
+      }
+      p.x = 200 + (i * segment)
+      p.y = 400 + (50 * m)
+    })
+  }
+
+  draw(layer, n) {
+    layer.draw.begin()
+    let cp1
+    let cp2
+    layer.draw.moveTo(this.points[0])
+    this.points.forEach((point, i) => {
+      let _point = point.clone()
+      if (i % 2 === 0) {
+        _point.y = point.y + (n * 100)
+      } else {
+        _point.y = point.y - (n * 100)
+      }
+      cp1 = _point.clone().moveBy(100, 0)
+      cp2 = _point.clone().moveBy(-100, 0)
+      layer.draw.bezierCurveTo(cp1, cp2, _point)
+    })
+    layer.draw.applyStyle({
+      noFill: true,
+      fillColor: 'white',
+      strokeWidth: 24,
+      strokeColor: 'white'
+    })
+    layer.draw.end()
+  }
+
+}
+
+let el_layer_stack = document.getElementById('layer_stack')
 let manager = new CanvasLayerManager(el_layer_stack)
 
 manager.create('main')
-console.log(ScreenModel.height)
-console.log(ScreenModel.width)
 
 let main = manager.find('main')
 main.draw.resolutionMultiplier = 2
 main.draw.resize(ScreenModel.width, ScreenModel.height)
-let circle_position = new Vector2(
+
+let center = new Vector2(
   ScreenModel.width / 2,
   ScreenModel.height / 2
 )
+
+let wave = new Wave()
+wave.initializePoints()
 
 let animation = new Animation({
   duration: 2,
@@ -35,9 +95,6 @@ let animation = new Animation({
   timingFunction: Easings.QuadEaseInEaseOut,
   onTick: n => {
     main.draw.clear()
-    main.draw.circle(circle_position, Num.modulate(n, 1, [0, 100]), {
-      fillColor: 'hsl(220, 100%, 50%)',
-      noStroke: true,
-    })
+    wave.draw(main, n)
   }
 }).play()
