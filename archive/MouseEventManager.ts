043f3fdded779843bddc69c1d13fa4e2a7868169
Andrew Prasetya <andrew@donkeymob.local>
2019-05-13 14:52:45 -0400

    Archive old codes

---
+++ archive/MouseEventManager.ts
@@ -0,0 +1,119 @@
+import {
+  MouseEventHandler,
+  Util,
+} from '../rocket/rocket'
+
+export interface MouseEventHandlers {
+  [name: string]: MouseEventHandler
+}
+
+export interface MouseEventManagerHook {
+  (
+    event: MouseEvent
+  ): void
+}
+
+export class MouseEventManager {
+
+  public onEvent: MouseEventManagerHook = (event) => { }
+
+  public onClick: MouseEventManagerHook = (event) => { }
+  public onDown: MouseEventManagerHook = (event) => { }
+  public onUp: MouseEventManagerHook = (event) => { }
+  public onMove: MouseEventManagerHook = (event) => { }
+
+  public debounce: Function
+  public debounceWait: number = 0.2
+
+  public handlers: MouseEventHandlers
+
+  constructor() {
+    this.handlers = {}
+  }
+
+  public register(name: string, handler: MouseEventHandler): this {
+    if (typeof this.handlers[name] === 'undefined') {
+      this.handlers[name] = handler
+      this.handlers[name].name = name
+    }
+    return this
+  }
+
+  public remove(name: string): this {
+    if (typeof this.handlers[name] === 'object') {
+      delete this.handlers[name]
+    }
+    return this
+  }
+
+  public find(name: string): MouseEventHandler | false {
+    if (typeof this.handlers[name] === 'object') {
+      return this.handlers[name]
+    }
+    return false
+  }
+
+  // EVENT HANDLER
+
+  private eventHandlerMouseClick = (event: MouseEvent) => {
+    this.onEvent(event)
+    this.onClick(event)
+    Object.keys(this.handlers).forEach(name => {
+      this.handlers[name].handleClick(event)
+    })
+  }
+
+  private eventHandlerMouseDown = (event: MouseEvent) => {
+    this.onEvent(event)
+    this.onDown(event)
+    Object.keys(this.handlers).forEach(name => {
+      this.handlers[name].handleDown(event)
+    })
+  }
+
+  private eventHandlerMouseUp = (event: MouseEvent) => {
+    this.onEvent(event)
+    this.onUp(event)
+    Object.keys(this.handlers).forEach(name => {
+      this.handlers[name].handleUp(event)
+    })
+  }
+
+  private eventHandlerMouseMove = (event: MouseEvent) => {
+    this.onEvent(event)
+    this.onMove(event)
+    Object.keys(this.handlers).forEach(name => {
+      this.handlers[name].handleMove(event)
+    })
+  }
+
+  private eventHandlerMouseMoveEnd = () => {
+    Object.keys(this.handlers).forEach(name => {
+      this.handlers[name].handleMoveEnd()
+    })
+  }
+
+  // LISTEN
+
+  public listen(): this {
+    this.debounce = Util.debounce(
+      this.debounceWait, this.eventHandlerMouseMoveEnd.bind(this)
+    )
+    window.addEventListener('click', this.eventHandlerMouseClick)
+    window.addEventListener('mousedown', this.eventHandlerMouseDown)
+    window.addEventListener('mouseup', this.eventHandlerMouseUp)
+    window.addEventListener('mousemove', this.eventHandlerMouseMove)
+    window.addEventListener('mousemove', <EventListener>this.debounce)
+    return this
+  }
+
+  public stopListen(): this {
+    window.removeEventListener('click', this.eventHandlerMouseClick)
+    window.removeEventListener('mousedown', this.eventHandlerMouseDown)
+    window.removeEventListener('mouseup', this.eventHandlerMouseUp)
+    window.removeEventListener('mousemove', this.eventHandlerMouseMove)
+    window.removeEventListener('mousemove', <EventListener>this.debounce)
+    return this
+  }
+
+}
