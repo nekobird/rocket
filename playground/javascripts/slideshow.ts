ca6198f001965a89f81822fbe75511c0288715d3
Andrew Prasetya <andrew@donkeymob.local>
2019-05-29 13:39:43 -0400

    Updated slideshow

--- playground/javascripts/slideshow.ts
+++ playground/javascripts/slideshow.ts
@@ -14,15 +14,17 @@ const data = jsondata;
 const gutter = 20;
 
 const maxHeight = () => {
-  return ViewportModel.height;
+  return 800;
 }
 
-const containerElement = document.querySelector('.js-slideshow-container');
+const containerElement = <HTMLElement>document.querySelector('.js-slideshow-container');
+let containerWidth: number = 0;
 
 const addImage = (url: string) => {
   const img = new Image();
   img.src = url;
 
+  updateContainerWidth(img);
   const item = document.createElement('DIV');
   item.classList.add('item');
   item.appendChild(img);
@@ -37,8 +39,21 @@ const loadItems = () => {
   });
 }
 
+const updateContainerWidth = (img) => {
+  const width = RectHelper.getWidthFromNewHeight(
+    { width: img.naturalWidth, height: img.naturalHeight },
+    maxHeight()
+  );
+  containerWidth += width + gutter;
+}
+
 const addItemToContainer = item => {
+  scaleContainer();
   containerElement.appendChild(item);
+}
+
+const scaleContainer = () => {
+  containerElement.style.width = `${containerWidth}px`;
 }
 
 loadItems();
