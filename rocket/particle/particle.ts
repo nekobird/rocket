e5b842531abd9e5c18cdbc6f77e56b4b09a19e02
Andrew Prasetya <andrew@donkeymob.local>
2019-07-05 10:19:29 -0400

    Updated particle.ts

--- rocket/particle/particle.ts
+++ rocket/particle/particle.ts
@@ -1,3 +1,32 @@
+import {
+  Vector2,
+} from '../rocket';
+
 export class Particle {
-  
+  public position: Vector2;
+  public velocity: Vector2;
+  public acceleration: Vector2;
+
+  public maximumSpeed: number = 10;
+
+  constructor() {
+    this.position = new Vector2;
+    this.velocity = new Vector2;
+    this.acceleration = new Vector2;
+  }
+
+  public applyForce(force: Vector2): this {
+    force.divide(1);
+    this.acceleration.add(force);
+    return this;
+  }
+
+  public update(): this {
+    this.velocity
+      .add(this.acceleration)
+      .limit(this.maximumSpeed);
+    this.position.add(this.velocity);
+    this.acceleration.multiply(0);
+    return this;
+  }
 }
