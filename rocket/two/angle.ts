7441fe96efd2b7c5b679b2f8825ce940be1f4248
Andrew Prasetya <andrewprasetya@andrew-imac.local>
2019-07-07 01:37:40 -0400

    Minor cleanups.

--- rocket/two/angle.ts
+++ rocket/two/angle.ts
@@ -14,10 +14,10 @@ export class Angle {
 
   public static deltaClockwise(from: number, to: number, direction: boolean = false): number {
     let sign = 1;
-    let range = from - Math.PI;
+    const range = from - Math.PI;
 
     if (range < 0) {
-      let offset = Num.cycle(range, Math.PI * 2);
+      const offset = Num.cycle(range, Math.PI * 2);
       if (to < from || to >= offset) sign = -1;
     } else if (to < from && to >= range ) {
       sign = -1;
@@ -26,22 +26,20 @@ export class Angle {
     if (direction === false) sign = 1;
 
     let result = 0;
-
     if (from > to) {
       result = from - to;
     } else if (to > from) {
       result = to - from;
     }
-
     return result * sign;
   }
 
   public static deltaCounterclockwise(from: number, to: number, direction: boolean = false): number {
     let sign = 1;
-    let range = from + Math.PI;
+    const range = from + Math.PI;
 
     if (range > Math.PI * 2) {
-      let offset = Num.cycle(range, Math.PI * 2);
+      const offset = Num.cycle(range, Math.PI * 2);
       if (to > from || to <= offset) sign = -1;
     } else if (to > from && to <= range) {
       sign = -1;
@@ -50,37 +48,31 @@ export class Angle {
     if (direction === false) sign = 1;
 
     let result = 0;
-
     if (from > to) {
       result = from - to;
     } else if (to > from) {
       result = to - from;
     }
-
     return result * sign;
   }
 
   public static differenceClockwise(from: number, to: number) {
     let result = 0;
-
     if (from > to) {
       result = (Math.PI * 2) - from + to;
     } else if (to > from) {
       result = to - from;
     }
-
     return result;
   }
 
   public static differenceCounterclockwise(from: number, to: number) {
     let result = 0;
-
     if (from > to) {
       result = from - to;
     } else if (to > from) {
       result = from + (Math.PI * 2) - to;
     }
-
     return result;
   }
 }
--- rocket/two/geo2Util.ts
+++ rocket/two/geo2Util.ts
@@ -30,12 +30,10 @@ export class Geo2Util {
   public static checkIfTwoLineSegmentsIntersect(ls1: Point, le1: Point, ls2: Point, le2: Point): boolean {
     const pi = Geo2Util.getPointOfIntersectionBetweenTwoLineSegments(ls1, le1, ls2, le2);
 
-    if (pi === false) {
-      return false;
-    }
+    if (pi === false) return false;
 
     if (
-      pi.x > Math.max(ls1.x, le1.x)
+         pi.x > Math.max(ls1.x, le1.x)
       || pi.x < Math.min(ls1.x, le1.x)
       || pi.x > Math.max(ls2.x, le2.x)
       || pi.x < Math.min(ls2.x, le2.x)
@@ -43,16 +41,14 @@ export class Geo2Util {
       || pi.y < Math.min(ls1.y, le1.y)
       || pi.y > Math.max(ls2.y, le2.y)
       || pi.y < Math.min(ls2.y, le2.y)
-    ) {
+    )
       return false;
-    }
     return true;
   }
 
   public static getPointOfIntersectionBetweenTwoLineSegments(ls1: Point, le1: Point, ls2: Point, le2: Point): Point | false {
-    if (Geo2Util.checkIfTwoLineSegmentsAreParallel(ls1, le1, ls2, le2) === true) {
+    if (Geo2Util.checkIfTwoLineSegmentsAreParallel(ls1, le1, ls2, le2) === true)
       return false;
-    }
 
     let ix = 0;
     let iy = 0;
@@ -61,8 +57,7 @@ export class Geo2Util {
     let m1 = Geo2Util.getSlopeOfLine(ls1, le1);
     let m2 = Geo2Util.getSlopeOfLine(ls2, le2);
 
-    let yi1;
-    let yi2;
+    let yi1, yi2;
 
     if (
       m1 === false
@@ -79,7 +74,7 @@ export class Geo2Util {
       yi1 = -1 * m1 * ls1.x + ls1.y;
       iy = m1 * ix + yi1;
     } else if (
-      typeof m1 === 'number'
+         typeof m1 === 'number'
       && typeof m2 === 'number'
     ) {
       // Get y-intercept
