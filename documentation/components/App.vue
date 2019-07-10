b949612999aebf7db401e04b7fc31aa6288281a7
Andrew Prasetya <andrewprasetya@andrew-imac.local>
2019-07-10 00:06:26 -0400

    Started vue project for documentation.

---
+++ documentation/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="site-content">
+    Hello {{message}}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      message: 'Hello World!'
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.site-content {
+  color: hsl(220, 100%, 25%);
+}
+</style>
