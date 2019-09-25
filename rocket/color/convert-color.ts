09c234108d0aabde69300d4fc4eab5df52f7e3a8
Andrew Prasetya <andrew@donkeymob.local>
2019-09-25 10:01:54 -0400

    Updated ConvertColor.

--- rocket/color/convert-color.ts
+++ rocket/color/convert-color.ts
@@ -25,203 +25,212 @@ type ColorArray3 = [number, number, number];
 
 type ColorArray4 = [number, number, number, number];
 
-export class ConvertColor {
-  // RGB 1, 1, 1
-  static RGBToHEX(...rgb: ColorArray3): string {
-    rgb[0] = Num.cycle(rgb[0], 1);
-    rgb[1] = Num.cycle(rgb[1], 1);
-    rgb[2] = Num.cycle(rgb[2], 1);
+// RGB 1, 1, 1
+export function RGBToHEX(...rgb: ColorArray3): string {
+  rgb[0] = Num.cycle(rgb[0], 1);
+  rgb[1] = Num.cycle(rgb[1], 1);
+  rgb[2] = Num.cycle(rgb[2], 1);
 
-    rgb = rgb.map(n => n * 255) as ColorArray3;
+  rgb = rgb.map(n => n * 255) as ColorArray3;
 
-    return `#${rgb[0].toString(16)}${rgb[1].toString(16)}${rgb[2].toString(16)}`;
+  return `#${rgb[0].toString(16)}${rgb[1].toString(16)}${rgb[2].toString(16)}`;
+}
+
+// RGB 1, 1, 1
+export function HEXToRGB(hex: string): ColorArray3 {
+  let rgb = [0, 0, 0];
+  let r = '';
+  let g = '';
+  let b = '';
+
+  if (hex.length === 7) {
+    r = hex.substr(1, 2);
+    g = hex.substr(3, 2);
+    b = hex.substr(5, 2);
+  } else if (hex.length === 4) {
+    r = hex.substr(1, 1);
+    g = hex.substr(2, 1);
+    b = hex.substr(3, 1);
+
+    r = r.concat(r);
+    g = g.concat(g);
+    b = b.concat(b);
   }
 
-  // RGB 1, 1, 1
-  static HEXToRGB(hex: string): ColorArray3 {
-    let rgb = [0, 0, 0];
-    let r = '';
-    let g = '';
-    let b = '';
+  rgb[0] = parseInt(r, 16);
+  rgb[1] = parseInt(g, 16);
+  rgb[2] = parseInt(b, 16);
 
-    if (hex.length === 7) {
-      r = hex.substr(1, 2);
-      g = hex.substr(3, 2);
-      b = hex.substr(5, 2);
-    } else if (hex.length === 4) {
-      r = hex.substr(1, 1);
-      g = hex.substr(2, 1);
-      b = hex.substr(3, 1);
+  return rgb.map(n => n / 255) as ColorArray3;
+}
 
-      r = r.concat(r);
-      g = g.concat(g);
-      b = b.concat(b);
-    }
+// CMYK 1, 1, 1, 1
+// RGB 1, 1, 1
+export function RGBToCMYK(...rgb: ColorArray3): ColorArray4 {
+  rgb[0] = Num.cycle(rgb[0], 1);
+  rgb[1] = Num.cycle(rgb[1], 1);
+  rgb[2] = Num.cycle(rgb[2], 1);
 
-    rgb[0] = parseInt(r, 16);
-    rgb[1] = parseInt(g, 16);
-    rgb[2] = parseInt(b, 16);
+  const cmyk = [0, 0, 0, 0];
 
-    return rgb.map(n => n / 255) as ColorArray3;
+  cmyk[3] = 1 - Math.max(...rgb);
+  cmyk[0] = (1 - rgb[0] - cmyk[3]) / (1 - cmyk[3]);
+  cmyk[1] = (1 - rgb[1] - cmyk[3]) / (1 - cmyk[3]);
+  cmyk[2] = (1 - rgb[2] - cmyk[3]) / (1 - cmyk[3]);
+
+  return cmyk as ColorArray4;
+}
+
+// CMYK 1, 1, 1, 1
+//  RGB 1, 1, 1
+export function CMYKToRGB(...cmyk: ColorArray4): ColorArray3 {
+  cmyk[0] = Num.cycle(cmyk[0], 1);
+  cmyk[1] = Num.cycle(cmyk[1], 1);
+  cmyk[2] = Num.cycle(cmyk[2], 1);
+  cmyk[3] = Num.cycle(cmyk[3], 1);
+
+  const rgb = [0, 0, 0];
+
+  rgb[0] = (1 - cmyk[0]) * (1 - cmyk[3]);
+  rgb[1] = (1 - cmyk[1]) * (1 - cmyk[3]);
+  rgb[2] = (1 - cmyk[2]) * (1 - cmyk[3]);
+
+  return rgb as ColorArray3;
+}
+
+// RGB   1, 1, 1
+// HSL 359, 1, 1
+export function RGBToHSL(...rgb: ColorArray3): ColorArray3 {
+  rgb[0] = Num.cycle(rgb[0], 1);
+  rgb[1] = Num.cycle(rgb[1], 1);
+  rgb[2] = Num.cycle(rgb[2], 1);
+
+  let cMin = Math.min(...rgb);
+  let cMax = Math.max(...rgb);
+  
+  let delta = cMax - cMin;
+
+  let hsl = [0, 0, 0];
+
+  if (delta === 0) {
+    hsl[0] = 0;
+  } else if (cMax === rgb[0]) {
+    hsl[0] = 60 * (((rgb[1] - rgb[2]) / delta) % 6);
+  } else if (cMax === rgb[1]) {
+    hsl[0] = 60 * ((rgb[2] - rgb[0]) / delta + 2);
+  } else if (cMax === rgb[2]) {
+    hsl[0] = 60 * ((rgb[0] - rgb[1]) / delta + 4);
   }
 
-  // CMYK 1, 1, 1, 1
-  // RGB 1, 1, 1
-  static RGBToCMYK(...rgb: ColorArray3): ColorArray4 {
-    rgb[0] = Num.cycle(rgb[0], 1);
-    rgb[1] = Num.cycle(rgb[1], 1);
-    rgb[2] = Num.cycle(rgb[2], 1);
+  hsl[2] = (cMax + cMin) / 2;
+  hsl[1] = delta === 0 ? 0 : delta / (1 - Math.abs(2 * hsl[2] - 1));
 
-    const cmyk = [0, 0, 0, 0];
+  return hsl as ColorArray3;
+}
 
-    cmyk[3] = 1 - Math.max(...rgb);
-    cmyk[0] = (1 - rgb[0] - cmyk[3]) / (1 - cmyk[3]);
-    cmyk[1] = (1 - rgb[1] - cmyk[3]) / (1 - cmyk[3]);
-    cmyk[2] = (1 - rgb[2] - cmyk[3]) / (1 - cmyk[3]);
+// HSL 359, 1, 1
+// RGB   1, 1, 1
+export function HSLToRGB(...hsl: ColorArray3): ColorArray3 {
+  hsl[0] = Num.cycle(hsl[0], 359);
+  hsl[1] = Num.cycle(hsl[1], 1);
+  hsl[2] = Num.cycle(hsl[2], 1);
 
-    return cmyk as ColorArray4;
+  let rgb = [0, 0, 0];
+
+  let h = hsl[0] / 60;
+
+  let c = (1 - Math.abs(2 * hsl[2] - 1)) * hsl[1];
+
+  let x = c * (1 - Math.abs((h % 2) - 1));
+
+  if (h >= 0 && h <= 1) {
+    rgb = [c, x, 0];
+  } else if (h >= 1 && h <= 2) {
+    rgb = [x, c, 0];
+  } else if (h >= 2 && h <= 3) {
+    rgb = [0, c, x];
+  } else if (h >= 3 && h <= 4) {
+    rgb = [0, x, c];
+  } else if (h >= 4 && h <= 5) {
+    rgb = [x, 0, c];
+  } else if (h >= 5 && h <= 6) {
+    rgb = [c, 0, x];
   }
 
-  // CMYK 1, 1, 1, 1
-  //  RGB 1, 1, 1
-  static CMYKToRGB(...cmyk: ColorArray4): ColorArray3 {
-    cmyk[0] = Num.cycle(cmyk[0], 1);
-    cmyk[1] = Num.cycle(cmyk[1], 1);
-    cmyk[2] = Num.cycle(cmyk[2], 1);
-    cmyk[3] = Num.cycle(cmyk[3], 1);
+  let m = hsl[2] - 0.5 * c;
 
-    const rgb = [0, 0, 0];
+  return rgb.map(v => v + m) as ColorArray3;
+}
 
-    rgb[0] = (1 - cmyk[0]) * (1 - cmyk[3]);
-    rgb[1] = (1 - cmyk[1]) * (1 - cmyk[3]);
-    rgb[2] = (1 - cmyk[2]) * (1 - cmyk[3]);
+// RGB   1, 1, 1
+// HSV 359, 1, 1
+export function RGBToHSV(...rgb: ColorArray3): ColorArray3 {
+  rgb[0] = Num.cycle(rgb[0], 1);
+  rgb[1] = Num.cycle(rgb[1], 1);
+  rgb[2] = Num.cycle(rgb[2], 1);
 
-    return rgb as ColorArray3;
+  let cMin = Math.min(...rgb);
+  let cMax = Math.max(...rgb);
+
+  let delta = cMax - cMin;
+
+  let hsv = [0, 0, 0];
+
+  if (delta === 0) {
+    hsv[0] = 0;
+  } else if (cMax === rgb[0]) {
+    hsv[0] = 60 * (((rgb[1] - rgb[2]) / delta) % 6);
+  } else if (cMax === rgb[1]) {
+    hsv[0] = 60 * ((rgb[2] - rgb[0]) / delta + 2);
+  } else if (cMax === rgb[2]) {
+    hsv[0] = 60 * ((rgb[0] - rgb[1]) / delta + 4);
   }
 
-  // RGB   1, 1, 1
-  // HSL 359, 1, 1
-  static RGBToHSL(...rgb: ColorArray3): ColorArray3 {
-    rgb[0] = Num.cycle(rgb[0], 1);
-    rgb[1] = Num.cycle(rgb[1], 1);
-    rgb[2] = Num.cycle(rgb[2], 1);
+  hsv[1] = cMax === 0 ? 0 : delta / cMax;
+  hsv[2] = cMax;
 
-    let cMin = Math.min(...rgb);
-    let cMax = Math.max(...rgb);
-    
-    let delta = cMax - cMin;
+  return hsv as ColorArray3;
+}
 
-    let hsl = [0, 0, 0];
+// HSV 359, 1, 1
+// RGB   1, 1, 1
+export function HSVToRGB(...hsv: ColorArray3): ColorArray3 {
+  hsv[0] = Num.cycle(hsv[0], 359);
+  hsv[1] = Num.cycle(hsv[1], 1);
+  hsv[2] = Num.cycle(hsv[2], 1);
 
-    if (delta === 0) {
-      hsl[0] = 0;
-    } else if (cMax === rgb[0]) {
-      hsl[0] = 60 * (((rgb[1] - rgb[2]) / delta) % 6);
-    } else if (cMax === rgb[1]) {
-      hsl[0] = 60 * ((rgb[2] - rgb[0]) / delta + 2);
-    } else if (cMax === rgb[2]) {
-      hsl[0] = 60 * ((rgb[0] - rgb[1]) / delta + 4);
-    }
+  const c = hsv[2] * hsv[1];
 
-    hsl[2] = (cMax + cMin) / 2;
-    hsl[1] = delta === 0 ? 0 : delta / (1 - Math.abs(2 * hsl[2] - 1));
+  const x = c * (1 - Math.abs(((hsv[0] / 60) % 2) - 1));
 
-    return hsl as ColorArray3;
+  let rgb = [0, 0, 0];
+
+  if (hsv[0] >= 0 && hsv[0] <= 60) {
+    rgb = [c, x, 0];
+  } else if (hsv[0] >= 60 && hsv[0] <= 120) {
+    rgb = [x, c, 0];
+  } else if (hsv[0] >= 120 && hsv[0] <= 180) {
+    rgb = [0, c, x];
+  } else if (hsv[0] >= 180 && hsv[0] <= 240) {
+    rgb = [0, x, c];
+  } else if (hsv[0] >= 240 && hsv[0] <= 300) {
+    rgb = [x, 0, c];
+  } else if (hsv[0] >= 300 && hsv[0] <= 360) {
+    rgb = [c, 0, x];
   }
 
-  // HSL 359, 1, 1
-  // RGB   1, 1, 1
-  static HSLToRGB(...hsl: ColorArray3): ColorArray3 {
-    hsl[0] = Num.cycle(hsl[0], 359);
-    hsl[1] = Num.cycle(hsl[1], 1);
-    hsl[2] = Num.cycle(hsl[2], 1);
+  const m = hsv[2] - c;
 
-    let rgb = [0, 0, 0];
+  return rgb.map(v => v + m) as ColorArray3;
+}
 
-    let h = hsl[0] / 60;
-
-    let c = (1 - Math.abs(2 * hsl[2] - 1)) * hsl[1];
-
-    let x = c * (1 - Math.abs((h % 2) - 1));
-
-    if (h >= 0 && h <= 1) {
-      rgb = [c, x, 0];
-    } else if (h >= 1 && h <= 2) {
-      rgb = [x, c, 0];
-    } else if (h >= 2 && h <= 3) {
-      rgb = [0, c, x];
-    } else if (h >= 3 && h <= 4) {
-      rgb = [0, x, c];
-    } else if (h >= 4 && h <= 5) {
-      rgb = [x, 0, c];
-    } else if (h >= 5 && h <= 6) {
-      rgb = [c, 0, x];
-    }
-
-    let m = hsl[2] - 0.5 * c;
-
-    return rgb.map(v => v + m) as ColorArray3;
-  }
-
-  // RGB   1, 1, 1
-  // HSV 359, 1, 1
-  static RGBToHSV(...rgb: ColorArray3): ColorArray3 {
-    rgb[0] = Num.cycle(rgb[0], 1);
-    rgb[1] = Num.cycle(rgb[1], 1);
-    rgb[2] = Num.cycle(rgb[2], 1);
-
-    let cMin = Math.min(...rgb);
-    let cMax = Math.max(...rgb);
-
-    let delta = cMax - cMin;
-
-    let hsv = [0, 0, 0];
-
-    if (delta === 0) {
-      hsv[0] = 0;
-    } else if (cMax === rgb[0]) {
-      hsv[0] = 60 * (((rgb[1] - rgb[2]) / delta) % 6);
-    } else if (cMax === rgb[1]) {
-      hsv[0] = 60 * ((rgb[2] - rgb[0]) / delta + 2);
-    } else if (cMax === rgb[2]) {
-      hsv[0] = 60 * ((rgb[0] - rgb[1]) / delta + 4);
-    }
-
-    hsv[1] = cMax === 0 ? 0 : delta / cMax;
-    hsv[2] = cMax;
-
-    return hsv as ColorArray3;
-  }
-
-  // HSV 359, 1, 1
-  // RGB   1, 1, 1
-  static HSVToRGB(...hsv: ColorArray3): ColorArray3 {
-    hsv[0] = Num.cycle(hsv[0], 359);
-    hsv[1] = Num.cycle(hsv[1], 1);
-    hsv[2] = Num.cycle(hsv[2], 1);
-
-    const c = hsv[2] * hsv[1];
-
-    const x = c * (1 - Math.abs(((hsv[0] / 60) % 2) - 1));
-
-    let rgb = [0, 0, 0];
-
-    if (hsv[0] >= 0 && hsv[0] <= 60) {
-      rgb = [c, x, 0];
-    } else if (hsv[0] >= 60 && hsv[0] <= 120) {
-      rgb = [x, c, 0];
-    } else if (hsv[0] >= 120 && hsv[0] <= 180) {
-      rgb = [0, c, x];
-    } else if (hsv[0] >= 180 && hsv[0] <= 240) {
-      rgb = [0, x, c];
-    } else if (hsv[0] >= 240 && hsv[0] <= 300) {
-      rgb = [x, 0, c];
-    } else if (hsv[0] >= 300 && hsv[0] <= 360) {
-      rgb = [c, 0, x];
-    }
-
-    const m = hsv[2] - c;
-
-    return rgb.map(v => v + m) as ColorArray3;
-  }
+export const ConvertColor = {
+  CMYKToRGB,
+  HEXToRGB,
+  HSLToRGB,
+  HSVToRGB,
+  RGBToCMYK,
+  RGBToHEX,
+  RGBToHSL,
+  RGBToHSV,
 }
